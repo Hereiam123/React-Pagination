9ab915cf5e89187da8ad799bcf6c281ed55ee942
hereiam123 <bdemaio1@gmail.com>
2019-07-21 21:29:33 -0400

    Setting up Hooks and Effect for loading in posts from Posts api

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const App = () => {
-  return <div className="App" />;
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage, setPostPerPage] = useState(10);
+
+  //Runs when component mounts. Empty array at end, stops run at mounting
+  useEffect(() => {
+    const fetchPosts = async () => {
+      setLoading(true);
+      const res = await axios.get('https://jsonplaceholder.typicode.com/posts');
+      setPosts(res.data);
+      setLoading(false);
+    };
+    fetchPosts();
+  }, []);
+
+  return (
+    <div className="container">
+      <h1>Pagination Station</h1>
+    </div>
+  );
 };
 
 export default App;
